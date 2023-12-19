6f0b0da2bf4310128bcee16380d9a6c253c3e389
Geovanny Camargo <92384356+Yisus3090@users.noreply.github.com>
2023-12-19 16:46:02 -0500

    montaje de la apagina

---
+++ html/assets/js/CambiarPrecios.js
@@ -0,0 +1,127 @@
+function PrecioEspecial(medida) {
+    var precioSpan = document.getElementById("especial");
+    
+    switch (medida) {
+      case "personal":
+        precioSpan.textContent = "$25.000";
+        break;
+      case "small":
+        precioSpan.textContent = "$30.000";
+        break;
+      case "medium":
+        precioSpan.textContent = "$35.000";
+        break;
+      case "large":
+        precioSpan.textContent = "$40.000";
+        break;
+      default:
+        break;
+    }
+  }
+
+  function PrecioSalami(medida) {
+    var precioSpan = document.getElementById("salami");
+    
+    switch (medida) {
+      case "personal":
+        precioSpan.textContent = "$25.000";
+        break;
+      case "small":
+        precioSpan.textContent = "$30.000";
+        break;
+      case "medium":
+        precioSpan.textContent = "$35.000";
+        break;
+      case "large":
+        precioSpan.textContent = "$40.000";
+        break;
+      default:
+        break;
+    }
+  }
+
+  function PrecioPollo(medida) {
+    var precioSpan = document.getElementById("pollo");
+    
+    switch (medida) {
+      case "personal":
+        precioSpan.textContent = "$25.000";
+        break;
+      case "small":
+        precioSpan.textContent = "$30.000";
+        break;
+      case "medium":
+        precioSpan.textContent = "$35.000";
+        break;
+      case "large":
+        precioSpan.textContent = "$40.000";
+        break;
+      default:
+        break;
+    }
+  }
+
+    function PrecioJamon(medida) {
+    var precioSpan = document.getElementById("jamon");
+    
+    switch (medida) {
+      case "personal":
+        precioSpan.textContent = "$25.000";
+        break;
+      case "small":
+        precioSpan.textContent = "$30.000";
+        break;
+      case "medium":
+        precioSpan.textContent = "$35.000";
+        break;
+      case "large":
+        precioSpan.textContent = "$40.000";
+        break;
+      default:
+        break;
+    }
+  }
+
+
+  function PrecioVegetariana(medida) {
+    var precioSpan = document.getElementById("vegetariana");
+    
+    switch (medida) {
+      case "personal":
+        precioSpan.textContent = "$25.000";
+        break;
+      case "small":
+        precioSpan.textContent = "$30.000";
+        break;
+      case "medium":
+        precioSpan.textContent = "$35.000";
+        break;
+      case "large":
+        precioSpan.textContent = "$40.000";
+        break;
+      default:
+        break;
+    }
+  }
+
+
+  function PrecioHawaiana(medida) {
+    var precioSpan = document.getElementById("hawaiana");
+    
+    switch (medida) {
+      case "personal":
+        precioSpan.textContent = "$25.000";
+        break;
+      case "small":
+        precioSpan.textContent = "$30.000";
+        break;
+      case "medium":
+        precioSpan.textContent = "$35.000";
+        break;
+      case "large":
+        precioSpan.textContent = "$40.000";
+        break;
+      default:
+        break;
+    }
+  }
